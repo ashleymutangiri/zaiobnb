6e2945665559018383fe3fbb1d2f03cb81fb9110
Ashley Farai <83772936+ashleymutangiri@users.noreply.github.com>
2026-07-07 10:02:24 -0700

    I wrapped the App component with CurrencyProvider

--- airbnb-frontend-clone - Copy/src/App.tsx
+++ airbnb-frontend-clone - Copy/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import { AuthProvider } from './context/AuthContext';
+import { CurrencyProvider } from './context/CurrencyContext';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -12,20 +13,22 @@ import AdminDashboard from './pages/AdminDashboard';
 export default function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <div className="min-h-screen flex flex-col font-sans text-gray-900 bg-white">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/reservations" element={<Reservations />} />
-            <Route path="/admin/*" element={<AdminDashboard />} />
-            <Route path="/locations/:locationId" element={<LocationList />} />
-            <Route path="/locations/:locationId/listing/:listingId" element={<LocationDetails />} />
-          </Routes>
-          <Footer />
-        </div>
-      </BrowserRouter>
+      <CurrencyProvider>
+        <BrowserRouter>
+          <div className="min-h-screen flex flex-col font-sans text-gray-900 bg-white">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/reservations" element={<Reservations />} />
+              <Route path="/admin/*" element={<AdminDashboard />} />
+              <Route path="/locations/:locationId" element={<LocationList />} />
+              <Route path="/locations/:locationId/listing/:listingId" element={<LocationDetails />} />
+            </Routes>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </CurrencyProvider>
     </AuthProvider>
   );
 }
